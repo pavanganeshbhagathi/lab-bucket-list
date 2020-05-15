6e8fc951150ee0f492bfdf37c3d954b3ced24bf6
pavanganeshbhagathi <pavanganeshbhagathi554@gmail.com>
2020-05-15 14:36:54 +0530

    last

--- src/service/ListOperations.java
+++ src/service/ListOperations.java
@@ -1,1 +1,57 @@
 package service;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import model.TouristPlace;
+
+public class ListOperations{
+	
+	
+	public List<TouristPlace> add(TouristPlace places)
+	{
+	        ArrayList<TouristPlace> al=new ArrayList<>();
+	        al.add(places);
+	        
+	        return al;
+		
+	}
+	public List<TouristPlace> remove(TouristPlace places)
+	{
+		ArrayList<TouristPlace> rl=new ArrayList<>();
+		rl.remove(places);
+		return null;
+		
+	}
+	public Object sortByDestination(List<TouristPlace> places)
+	{
+		
+		List<TouristPlace> list=new ArrayList<>();
+		
+		List<TouristPlace>  sortedlist=list.stream().sorted((d1,d2)->d1.getDestination().compareTo(d2.getDestination())).collect(Collectors.toList());
+		return sortedlist;
+
+	}
+	public Object sortByRank(List<TouristPlace> places)
+	{
+		ArrayList<TouristPlace> list=new ArrayList<>();
+/*Comparator<Integer> c=(a,b)->(a.getRank()<b.getRank())?-1:(a.getRank()>b.getRank())?1:0;
+		
+		 Collections.sort(list,c);*/
+		
+		list.sort((a,b)->a.getRank().compareTo(b.getRank()));
+		return list;
+		
+	}
+	public Object reset(List<TouristPlace> places)
+	{
+		
+		ArrayList<TouristPlace> list=new ArrayList<>();
+		list.clear();
+		return list;
+		
+	}
+}
--- src/service/MapOperations.java
+++ src/service/MapOperations.java
@@ -1,2 +1,52 @@
 package service;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.TreeMap;
+
+import model.TouristPlace;
+
+public class MapOperations 
+{
+	Map<Integer,TouristPlace> add(TouristPlace places)
+	{
+		Map<Integer,TouristPlace> map=new HashMap<>();
+		map.put(null, places);
+		return map;
+		
+	}
+	Map<Integer,TouristPlace> sortRandomly(TouristPlace places)
+	{
+		Map<Integer,TouristPlace> map=new HashMap<>();
+		map.put(null, places);
+		return map;
+	
+		
+		
+	}
+
+	public Object reset(HashMap<Integer,TouristPlace> places) {
+		
+		Map<Integer,TouristPlace> resetPlaces=new TreeMap<>();
+		resetPlaces.clear();
+		return resetPlaces;
+		
+	}
+	public Map<Integer, TouristPlace> remove(TouristPlace places){
+		
+		Map<Integer,TouristPlace> map=new HashMap<>();
+		map.remove(places);
+		return map;
+	}
+	public Map<Integer,TouristPlace> sortAlphabetically(TouristPlace places){
+		Map<Integer,TouristPlace> sortAlphabeticallyPlaces=new TreeMap<>();
+		 sortAlphabeticallyPlaces.put( null, places);
+		return  sortAlphabeticallyPlaces;
+		
+	}
+	
+}
